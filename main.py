c6da10ca0a214e9bef93b5fd937e33aeea6b8719
smurilloo <143961450+smurilloo@users.noreply.github.com>
2025-07-21 16:15:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -84,9 +84,3 @@ async def inspect():
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    import os
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
-
