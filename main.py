91934807b4f25637e51d2e8e58ebcfd60c62fefc
smurilloo <143961450+smurilloo@users.noreply.github.com>
2025-07-25 10:00:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -82,9 +82,3 @@ async def inspect():
 
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
-
